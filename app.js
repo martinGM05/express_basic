7f89dd335d8a7eee3c4ca4606e907661cad5c1b2
Martin Gonzalez Miranda <mtn.gm05@gmail.com>
2022-04-21 21:14:01 -0500

    Added launchx path

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+
+app.get('/launchx', (req, res) => {
+    res.send('Bienvenidos a launchx')
+})
+
+
 // With this initialize this app
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
